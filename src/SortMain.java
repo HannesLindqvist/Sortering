9a46b456983bca91a24f740280bb22650de6778c
HannesLindqvist <Hannes.Lindqvist@outlook.com>
2021-11-11 11:05:41 +0100

    Har kodat skalet på uppgiften.

--- src/SortMain.java
+++ src/SortMain.java
@@ -3,5 +3,6 @@ public class SortMain {
     public static void main(String[] args) {
         // Hämta in data från olika klasser.
         System.out.println("Hello");
+        //addar förändring för git
     }
 }
