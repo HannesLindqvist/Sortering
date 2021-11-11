0b329993148da242787d74fcc73a781fe6cf62fc
Hannes Lindqvist <92028400+HannesLindqvist@users.noreply.github.com>
2021-11-11 11:38:13 +0100

    Add files via upload
    Jobbar på skalet för uppgiften

---
+++ SortMain.java
@@ -0,0 +1,8 @@
+public class SortMain {
+
+    public static void main(String[] args) {
+        // Hämta in data från olika klasser.
+        System.out.println("Hello");
+        //addar förändring för git
+    }
+}
